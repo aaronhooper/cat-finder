e342e25bc2e29e6330ad6df9223c9b69be507c19
Aaron Hooper <rnjckhpr@gmail.com>
2022-06-20 14:11:37 +0200

    Add non-parallel cat finder functions

--- lib/cat_finder.ex
+++ lib/cat_finder.ex
@@ -9,6 +9,21 @@ defmodule CatFinder do
     send(sender, {filename, result})
   end
 
+  def count_cats_sync(filename) do
+    result =
+      filename
+      |> File.read!()
+      |> String.split("\s")
+      |> Enum.count(&(&1 == "cat"))
+
+    {filename, result}
+  end
+
+  def run_sync(filenames) do
+    filenames
+    |> Enum.map(fn filename -> count_cats_sync(filename) end)
+  end
+
   def run(filenames) do
     filenames
     |> Enum.map(fn filename ->
